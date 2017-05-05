ece57f96f10861d1ed10b38af8bcd2d1f4f9e4c5
italofeitosa <itinhofeiosa@gmail.com>
2017-05-05 13:57:07 -0300

    Criação do projeto Easy Movie Server.

---
+++ src/main/java/com/fiaf/easymovie/EasymovieApplication.java
@@ -0,0 +1,12 @@
+package com.fiaf.easymovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymovieApplication.class, args);
+	}
+}
